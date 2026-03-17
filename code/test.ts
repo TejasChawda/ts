0cc0c4d51a4c220430e4ad75ad38d092ba4a9fa6
TEJAS <tejasc@testvagrant.com>
2026-03-17 17:22:57 +0530

    tsconfig, optional chaining, bang

--- code/test.ts
+++ code/test.ts
@@ -1,2 +1,13 @@
 const x: number = 3;
 console.log(x);
+
+//*************************************** */
+
+//Optional Chaining and bang
+//So here what it means is Optional chaining will move to the chained operation only if the value exists
+//But the bang will move up the chain even if the value is present or undefined
+
+const arr = [[{"name":"john"}]];
+
+const res = arr.pop()?.pop()?.name; //Optional chaining
+const res1 = arr.pop()!.pop()!.name; //bang
